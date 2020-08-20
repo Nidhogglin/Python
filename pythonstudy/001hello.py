7746fa585114f928c6cfed681ac77ceba386ef43
Nidhogglin <191490681@qq.com>
2020-08-20 23:40:56 +0800

    新增项目文件

--- pythonstudy/001hello.py
+++
@@ -1,5 +0,0 @@
-print("Hello VScode!")
-
-a = input("请输入：")
-
-print("你输入的是：", a)
--- pythonstudy/005list.py
+++
@@ -1,8 +0,0 @@
-# 列表定义、初始化
-a_list = ['abc', 'xyz']
-# 增加元素
-a_list.append('Lin')
-print(a_list)
-# 删除元素
-a_list.remove('xyz')
-print(a_list)
